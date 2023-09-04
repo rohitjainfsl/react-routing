ff392a51c43d85dfc285ac418dd089245b69db6b
Rohit Jain <100214764+rohitjainfsl@users.noreply.github.com>
2023-09-04 14:05:23 +0530

    Initial commit

---
+++ src/Products.jsx
@@ -0,0 +1,61 @@
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+
+function Products() {
+  const [products, setProducts] = useState([]);
+  const [cart, setCart] = useState([]);
+
+  useEffect(() => {
+    axios.get("https://fakestoreapi.com/products").then((response) => {
+      setProducts(response.data);
+    });
+  }, []);
+  function handleAddToCart(e, product) {
+    e.preventDefault();
+    setCart([...cart, product]);
+  }
+  function checkProductInCart(ind) {
+    const itemInCart = cart.find((item, index) => {
+      return index === ind;
+    });
+    return itemInCart === undefined ? false : true;
+  }
+
+  function handleRemoveFromCart(e, product) {
+    e.preventDefault();
+    setCart(
+      cart.filter((item) => {
+        return item.id !== product.id;
+      })
+    );
+  }
+
+  console.log(cart);
+  return (
+    <div className="products content">
+      <h2>Products</h2>
+      <div className="product-wrapper">
+        {products.map((product, index) => {
+          return (
+            <div className="product" key={index}>
+              <img src={product.image} alt="Product Photo" />
+              <h3>{product.title}</h3>
+              <p>{product.price}</p>
+              {checkProductInCart(index) ? (
+                <a href="" onClick={(e) => handleRemoveFromCart(e, product)}>
+                  Remove From Cart
+                </a>
+              ) : (
+                <a href="" onClick={(e) => handleAddToCart(e, product)}>
+                  Add To Cart
+                </a>
+              )}
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
+}
+
+export default Products;
