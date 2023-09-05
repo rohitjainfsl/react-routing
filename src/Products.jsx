a150367f300a4dff1b1d360b4505d47e5ddde098
fullstacklearning <100214764+rohitjainfsl@users.noreply.github.com>
2023-09-05 07:20:29 +0000

    Added cart at the side

--- src/Products.jsx
+++ src/Products.jsx
@@ -1,9 +1,12 @@
 import axios from "axios";
 import React, { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+import ShoppingCartIcon from "@mui/icons-material/ShoppingCart";
 
 function Products() {
   const [products, setProducts] = useState([]);
   const [cart, setCart] = useState([]);
+  const [cartInView, setCartInView] = useState(false);
 
   useEffect(() => {
     axios.get("https://fakestoreapi.com/products").then((response) => {
@@ -29,33 +32,69 @@ function Products() {
       })
     );
   }
-  return (
-    <div className="products content">
-      <h2>Products</h2>
-      <div className="product-wrapper">
-        {
-          products.map((product, index) => {
-              return (
-                <div className="product" key={index}>
-                  <img src={product.image} alt="Product Photo" />
-                  <h3>{product.title}</h3>
-                  <p>{product.price}</p>
-                  {checkProductInCart(product) ? (
-                    <a href="" onClick={(e) => handleRemoveFromCart(e, product)}>
-                      Remove From Cart
-                    </a>
-                  ) : (
-                    <a href="" onClick={(e) => handleAddToCart(e, product)}>
-                      Add To Cart
-                    </a>
-                  )}
-                </div>
-              );
-            })
-        }
-        
+
+  function slideCartIntoView() {
+    setCartInView(!cartInView);
+  }
+
+  console.log(cart)
+
+  function Cart() {
+    return (
+      <div
+        className="cart sticky"
+        style={{ right: cartInView ? "0" : "-250px" }}
+      >
+        <div className="handle" onClick={slideCartIntoView}>
+          Cart <ShoppingCartIcon />
+        </div>
+        <ul className="cart-items">
+          {cart.map((item, index) => {
+            return (
+              <li key={index}>
+                {
+                  <>
+                    <img src={item.image} alt="Cart Product Photo" />
+                    <span>{item.title}</span>
+                  </>
+                }
+              </li>
+            );
+          })}
+        </ul>
       </div>
-    </div>
+    );
+  }
+
+  return (
+    <>
+      <Cart />
+      <div className="products content">
+        <h2>Products</h2>
+        <div className="product-wrapper">
+          {products.map((product, index) => {
+            return (
+              <div className="product" key={index}>
+                <Link to={"/products/" + index}>
+                  <img src={product.image} alt="Product Photo" />
+                </Link>
+                <h3>{product.title}</h3>
+                <p>{product.price}</p>
+                {checkProductInCart(product) ? (
+                  <a href="" onClick={(e) => handleRemoveFromCart(e, product)}>
+                    Remove From Cart
+                  </a>
+                ) : (
+                  <a href="" onClick={(e) => handleAddToCart(e, product)}>
+                    Add To Cart
+                  </a>
+                )}
+              </div>
+            );
+          })}
+        </div>
+      </div>
+    </>
   );
 }
 
